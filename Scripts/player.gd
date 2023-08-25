08833c5a9080f26f446e2f32466c962f74fd82bb
Nallekari <tuomas.nallikari@outlook.com>
2023-08-25 10:51:26 +0300

    added functionan enemy, enemy_spawner and added collision function between rockets and enemies

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,6 +2,11 @@ extends CharacterBody2D
 
 @export var Speed = 300
 
+var rocket_scene = preload("res://Scenes/rocket.tscn")
+
+@onready var rocket_container = $RocketContainer
+
+
 func _physics_process(delta):
 	velocity = Vector2(0,0)
 	if Input.is_action_pressed("move_right"):
@@ -13,19 +18,19 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_down"):
 		velocity.y = Speed
 	
-	
 	move_and_slide()
 	
 	var screen_size = get_viewport_rect().size
-#	if global_position.x < 0:
-#		global_position.x = 0
-#	if global_position.x > screen_size.x:
-#		global_position.x = screen_size.x
-#	if global_position.y > screen_size.y:
-#		global_position.y = screen_size.y
-#	if global_position.y < 0:
-#		global_position.y = 0
-#	global_position.x = clampf(global_position.x, 0, screen_size.x)
-#	global_position.y = clampf(global_position.y, 0, screen_size.y)
 	
 	global_position = global_position.clamp(Vector2(0,0), screen_size)
+
+func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+		
+func shoot():
+	var rocket_instance = rocket_scene.instantiate()
+	rocket_container.add_child(rocket_instance)
+	rocket_instance.global_position = global_position
+	rocket_instance.global_position.x += 80
+
